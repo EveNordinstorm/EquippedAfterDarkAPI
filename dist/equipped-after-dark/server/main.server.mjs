39d8f79c1d7cd5837354fb0831bec8146cf07ce3
evenordinstorm <evenordinstorm@gmail.com>
2025-08-30 19:04:56 +0100

    update: swap to local build

--- dist/equipped-after-dark/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FNVJYQMH.mjs";import"./chunk-KZ4TJ6W4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- index.js
+++ index.js
@@ -9,16 +9,18 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 const path = require("path");
 
+// Serve static files
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
+
 // Use CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
 
 // Middleware to parse JSON
 app.use(express.json());
-
-// Serve static files from the Angular client folder
-app.use(
-  express.static(path.join(__dirname, "dist", "equipped-after-dark", "browser"))
-);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -33,19 +35,6 @@ db.once("open", () => console.log("Connected to MongoDB"));
 // Define API routes
 app.use("/api", productRoutes);
 app.use("/api/users", userRoutes);
-
-// Serve the Angular app for the root URL and all other routes
-app.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "dist", "equipped-after-dark", "browser", "index.html")
-  );
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "dist", "equipped-after-dark", "browser", "index.html")
-  );
-});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
